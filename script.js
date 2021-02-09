4636b1a28e821ff68339344b4c136033a3494a00
Kes <74243456+Kes-trel@users.noreply.github.com>
2021-02-09 15:13:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
+// IE not rendering the site properly
+// If you see this code it means that I'm still working on it to fix it.
+//It seems that IE does not like CSS Grid and not reading JavaScript in ES6 with arrow functions etc.
+// I hope you'll give me some slack for this :) Thanks
+
 const navItems = document.querySelectorAll(".navlink");
 const navButton = document.querySelectorAll(".nav-button");
 const allImages = document.querySelectorAll(".image")
 const allText = document.querySelectorAll(".text");
 
 
-function toggleText(dataClass){
-  allText.forEach( text => {
-  text.dataset.class === dataClass ? text.style.display = "block" : text.style.display = "none";
+toggleText = (dataClass) => {
+  allText.forEach(text => {
+    text.dataset.class === dataClass ? text.style.display = "block" : text.style.display = "none";
   });
 
-  allImages.forEach( image => {
-  image.dataset.class === dataClass ? image.style.display = "block" : image.style.display = "none";
+  allImages.forEach(image => {
+    image.dataset.class === dataClass ? image.style.display = "block" : image.style.display = "none";
   })
 
 }
 
-navItems.forEach( function(item) {
+navItems.forEach(function (item) {
   item.addEventListener("click", () => {
     toggleText(item.dataset.class);
   });
 });
 
-navButton.forEach( function(item) {
+navButton.forEach(function (item) {
   item.addEventListener("click", () => {
     toggleText(item.dataset.class);
   });
@@ -30,23 +35,22 @@ navButton.forEach( function(item) {
 const allLinks = document.querySelectorAll(".header a");
 const scrollDiv = document.getElementById("text-scroll")
 
-allLinks.forEach( function(item) {
+allLinks.forEach(function (item) {
   item.addEventListener("click", () => {
-    scrollDiv.scrollTop = 0;  
+    scrollDiv.scrollTop = 0;
   });
 });
 
 
 // OVERLAY FOR CONTACT ME MENU - START
 
-function openOverlay() {
-    document.getElementById("contactNav").style.width = "100%";
-}
-  
-function closeOverlay() {
-    document.getElementById("contactNav").style.width = "0%";
+openOverlay = () => {
+  document.getElementById("contactNav").style.width = "100%";
 }
 
+closeOverlay = () => {
+  document.getElementById("contactNav").style.width = "0%";
+}
 
 // Links for contact buttons
 
@@ -68,10 +72,6 @@ document.getElementById("github").onclick = () => {
 
 document.getElementById("twitter").onclick = () => {
   window.open("https://twitter.com/sidagis");
-}
-
-document.getElementById("skype").onclick = () => {
-  window.open("https://join.skype.com/invite/hahCJc2s8ef6");
 }
 
 // Links for contact buttons
