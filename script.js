ece011c7805e2d1cecafe0216948e9b1110780e2
Kes <74243456+Kes-trel@users.noreply.github.com>
2021-02-23 09:30:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-// IE not rendering the site properly
-// If you see this code it means that I'm still working on it to fix it.
-//It seems that IE does not like CSS Grid and not reading JavaScript in ES6 with arrow functions etc.
-// I hope you'll give me some slack for this :) Thanks
-
 const navItems = document.querySelectorAll(".navlink");
 const navButton = document.querySelectorAll(".nav-button");
-const allImages = document.querySelectorAll(".image")
+const allImages = document.querySelectorAll(".image");
 const allText = document.querySelectorAll(".text");
 
-
 toggleText = (dataClass) => {
-  allText.forEach(text => {
-    text.dataset.class === dataClass ? text.style.display = "block" : text.style.display = "none";
+  allText.forEach((text) => {
+    text.dataset.class === dataClass
+      ? (text.style.display = "block")
+      : (text.style.display = "none");
   });
 
-  allImages.forEach(image => {
-    image.dataset.class === dataClass ? image.style.display = "block" : image.style.display = "none";
-  })
-
-}
+  allImages.forEach((image) => {
+    image.dataset.class === dataClass
+      ? (image.style.display = "block")
+      : (image.style.display = "none");
+  });
+};
 
 navItems.forEach(function (item) {
   item.addEventListener("click", () => {
@@ -33,7 +30,7 @@ navButton.forEach(function (item) {
 });
 
 const allLinks = document.querySelectorAll(".header a");
-const scrollDiv = document.getElementById("text-scroll")
+const scrollDiv = document.getElementById("text-scroll");
 
 allLinks.forEach(function (item) {
   item.addEventListener("click", () => {
@@ -41,38 +38,37 @@ allLinks.forEach(function (item) {
   });
 });
 
-
 // OVERLAY FOR CONTACT ME MENU - START
 
 openOverlay = () => {
   document.getElementById("contactNav").style.width = "100%";
-}
+};
 
 closeOverlay = () => {
   document.getElementById("contactNav").style.width = "0%";
-}
+};
 
 // Links for contact buttons
 
 document.getElementById("mobile").onclick = () => {
   window.open("tel: +447742471147");
-}
+};
 
 document.getElementById("email").onclick = () => {
   window.open("mailto:kes.sidagis@gmail.com");
-}
+};
 
 document.getElementById("linkedin").onclick = () => {
   window.open("https://www.linkedin.com/in/sidagis/");
-}
+};
 
 document.getElementById("github").onclick = () => {
   window.open("https://github.com/Kes-trel");
-}
+};
 
 document.getElementById("twitter").onclick = () => {
   window.open("https://twitter.com/sidagis");
-}
+};
 
 // Links for contact buttons
 
